a52541f27ac75f33d809af8ad688a06bb89b6640
Robert Ban <1304dante@gmail.com>
2023-03-07 13:05:02 +0100

    no message

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 import express, { Express} from 'express';
 import homeRouter from "./api/v1/home/home";
-  
+import bodyParser from "body-parser";
+import  swaggerJsdoc from "swagger-jsdoc";
+import  swaggerUi from "swagger-ui-express";
+import { SWAGGER_OPTIONS } from './consts/swagger.options';
 
 const PORT = 3000;
 
 const app: Express = express();
 
+const specs = swaggerJsdoc(SWAGGER_OPTIONS);
 
-
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs, { explorer: true })
+);
 app.use('/home', homeRouter);
 
 
